0d0871a2444a755a73426b468a828a75b5927cbf
Taiga Takano <ttttghghnb554z@outlook.jp>
2025-08-16 22:35:26 +0900

    Add dockerfile

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-# docker build -t mlops-cloud-backend-cv:latest -f ./docker/Dockerfile.cv .
-docker build -t mlops-cloud-backend-mlx:latest -f ./docker/Dockerfile.mlx .
+docker build -t mlops-cloud-backend-cv:latest -f ./Dockerfile.cv .
+docker build -t mlops-cloud-backend-mlx:latest -f ./Dockerfile.mlx .
