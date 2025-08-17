fe2e564bd3736c99e8427d9ae4681a76a1594241
Taiga Takano <ttttghghnb554z@outlook.jp>
2025-08-18 00:34:00 +0900

    add hm

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 # ===== 設定 =====
 NAMESPACE="taigatakano"
 PROJECT="mlops-cloud-backend"
-VARIANTS=(cv mlx)             # ビルド対象
+VARIANTS=(cv mlx hm)             # ビルド対象
 CONTEXT="."                   # ビルドコンテキスト
 VERSION="${VERSION:-${1:-v1.0}}"   # 環境変数 VERSION または第1引数で上書き可
 BUILDKIT="${BUILDKIT:-1}"     # DOCKER_BUILDKIT=1 をデフォルト
